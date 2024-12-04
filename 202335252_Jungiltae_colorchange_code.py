040b0d95603932f6c9470f249b42f566d0f8fb13
jit-github919 <jit214210@naver.com>
2024-12-04 22:14:03 +0900

    Update 202335252_Jungiltae_colorchange_code.py

--- 202335252_Jungiltae_colorchange_code.py
+++ 202335252_Jungiltae_colorchange_code.py
@@ -16,9 +16,9 @@ def extract_regions(image, cloth_lower, cloth_upper, bg_lower, bg_upper):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
    
-    cloth_mask_1 = cv2.inRange(hsv, cloth_lower[0], cloth_upper[0])  # 첫 번째 범위
-    cloth_mask_2 = cv2.inRange(hsv, cloth_lower[1], cloth_upper[1])  # 두 번째 범위
-    cloth_mask = cv2.bitwise_or(cloth_mask_1, cloth_mask_2)  # 두 마스크를 합침
+    cloth_mask_1 = cv2.inRange(hsv, cloth_lower[0], cloth_upper[0])  
+    cloth_mask_2 = cv2.inRange(hsv, cloth_lower[1], cloth_upper[1])  
+    cloth_mask = cv2.bitwise_or(cloth_mask_1, cloth_mask_2)  
     cloth_region = cv2.bitwise_and(image, image, mask=cloth_mask)
 
     
@@ -38,12 +38,12 @@ def main():
         return
 
    
-    cloth_lower = [np.array([140, 50, 50]), np.array([170, 50, 50])]  # 핑크색 하한값
-    cloth_upper = [np.array([170, 255, 255]), np.array([180, 255, 255])]  # 핑크색 상한값
+    cloth_lower = [np.array([140, 50, 50]), np.array([170, 50, 50])]  
+    cloth_upper = [np.array([170, 255, 255]), np.array([180, 255, 255])]  
 
    
-    bg_lower = np.array([0, 0, 0])  # 검정색 하한값 (Saturation과 Value가 낮은 범위)
-    bg_upper = np.array([180, 50, 50])  # 검정색 상한값 (Saturation과 Value가 낮은 범위)
+    bg_lower = np.array([0, 0, 0])  
+    bg_upper = np.array([180, 50, 50])  
 
    
     cloth_region, bg_region = extract_regions(image, cloth_lower, cloth_upper, bg_lower, bg_upper)
